bf9bcab8a30b05244378fbbafb61a0b303779db7
prabeeshnetstager <59558377+prabeeshnetstager@users.noreply.github.com>
2022-05-30 10:07:24 +0530

    second commit

--- api/view_pets.php
+++ api/view_pets.php
@@ -1,20 +1,17 @@
 <?php
-include 'conn.php';
+include ('conn.php');
 $query=mysqli_query($conn,"SELECT * FROM pet_tb");
-
+$data=array();
 while($row=mysqli_fetch_assoc($query))
-
-if($query)
 {
-    $array['msg']='done';
-    $array['data']=$row['owner_name'];
-    $array['data1']=$row['mobile'];
-    $array['data2']=$row['address'];
-    $array['data3']=$row['price'];
+    $array['name']=$row['owner_name'];
+    $array['mobile']=$row['mobile'];
+    $array['address']=$row['address']; 
+    $array['image']=$row['image'];
+    $array['category']=$row['category'];
+    $array['info']=$row['information'];
+    $array['price']=$row['price'];
+    array_push($data,$array);
 }
-else
-{
-    $array['msg']='fail';
-}
-echo json_encode($array);
+echo json_encode($data);
 ?>
